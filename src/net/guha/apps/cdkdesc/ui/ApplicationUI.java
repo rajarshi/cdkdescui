edc19e7df0452768002ca789f12a5ff56afbac4b
rajarshi <rajarshi@c8f1b752-e072-43e1-b38a-543abc4c3ac1>
2007-09-16 01:59:28 +0000

    Fixed typos
    git-svn-id: http://rguha.ath.cx/svn/src/java/CDKDescUI@11 c8f1b752-e072-43e1-b38a-543abc4c3ac1

--- src/net/guha/apps/cdkdesc/ui/ApplicationUI.java
+++ src/net/guha/apps/cdkdesc/ui/ApplicationUI.java
@@ -173,9 +173,10 @@ public class ApplicationUI {
                 }
             }
         }
+    }
 
-        public static void main
-        (String[]args){
+
+    public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel(
                     UIManager.getSystemLookAndFeelClassName());
@@ -189,7 +190,7 @@ public class ApplicationUI {
         frame.setVisible(true);
     }
 
+}
 
-    }
 
 
