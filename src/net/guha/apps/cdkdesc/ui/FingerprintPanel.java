8720215ac21fbcdd80c148d99cec2c16e044a19f
rajarshi <rajarshi@c8f1b752-e072-43e1-b38a-543abc4c3ac1>
2008-07-23 23:41:44 +0000

    Initial code for a fingerprint pane
    git-svn-id: http://rguha.ath.cx/svn/src/java/CDKDescUI@58 c8f1b752-e072-43e1-b38a-543abc4c3ac1

---
+++ src/net/guha/apps/cdkdesc/ui/FingerprintPanel.java
@@ -0,0 +1,13 @@
+package net.guha.apps.cdkdesc.ui;
+
+import javax.swing.*;
+
+/**
+ * @author Rajarshi Guha
+ */
+public class FingerprintPanel extends JPanel {
+
+    public FingerprintPanel() {
+        super();
+    }
+}
