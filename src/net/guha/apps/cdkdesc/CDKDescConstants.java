6362364c038005f90c99e9cee1f6bd7fbd3e4a78
rajarshi <rajarshi@c8f1b752-e072-43e1-b38a-543abc4c3ac1>
2008-07-25 17:15:45 +0000

    Added a constant for ARFF format
    git-svn-id: http://rguha.ath.cx/svn/src/java/CDKDescUI@77 c8f1b752-e072-43e1-b38a-543abc4c3ac1

--- src/net/guha/apps/cdkdesc/CDKDescConstants.java
+++ src/net/guha/apps/cdkdesc/CDKDescConstants.java
@@ -10,6 +10,7 @@ public class CDKDescConstants {
     public static final String OUTPUT_CSV = "CSV";
     public static final String OUTPUT_TAB = "TAB";
     public static final String OUTPUT_SPC = "SPC";
+    public static final String OUTPUT_ARFF = "ARFF";
     public static final String OUTPUT_SDF = "SDF";
     public static final String OUTPUT_CML = "CML";
 
