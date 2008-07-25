a3df2d1101aa29ecf51be8ac97389175cdb1c44b
rajarshi <rajarshi@c8f1b752-e072-43e1-b38a-543abc4c3ac1>
2008-07-25 16:52:06 +0000

    Added an interface for different text formats. Also created a directory for intrfaes
    git-svn-id: http://rguha.ath.cx/svn/src/java/CDKDescUI@72 c8f1b752-e072-43e1-b38a-543abc4c3ac1

---
+++ src/net/guha/apps/cdkdesc/ITextOutput.java
@@ -0,0 +1,11 @@
+package net.guha.apps.cdkdesc;
+
+public interface ITextOutput {
+
+    public void writeHeader(String header);
+
+    public void writeLine(String line);
+
+    public void writeFooter(String line);
+
+}
