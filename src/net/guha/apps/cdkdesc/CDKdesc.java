f72d5a59c901e9a241fb97a3c2cb7cf44c26e55b
Rajarshi  Guha <rajarshi.guha@gmail.com>
2010-04-04 12:12:37 -0400

    Updated verbose output for CLI calculations

--- src/net/guha/apps/cdkdesc/CDKdesc.java
+++ src/net/guha/apps/cdkdesc/CDKdesc.java
@@ -1,6 +1,8 @@
 package net.guha.apps.cdkdesc;
 
 import net.guha.apps.cdkdesc.interfaces.ISwingWorker;
+import net.guha.apps.cdkdesc.interfaces.ITextOutput;
+import net.guha.apps.cdkdesc.output.PlainTextOutput;
 import net.guha.apps.cdkdesc.ui.ApplicationMenu;
 import net.guha.apps.cdkdesc.ui.ApplicationUI;
 import net.guha.apps.cdkdesc.ui.DescriptorTree;
@@ -15,8 +17,22 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
+import org.openscience.cdk.CDKConstants;
+import org.openscience.cdk.DefaultChemObjectBuilder;
 import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.interfaces.IMolecule;
+import org.openscience.cdk.io.iterator.DefaultIteratingChemObjectReader;
+import org.openscience.cdk.io.iterator.IteratingMDLReader;
+import org.openscience.cdk.io.iterator.IteratingSMILESReader;
+import org.openscience.cdk.qsar.DescriptorEngine;
+import org.openscience.cdk.qsar.DescriptorValue;
 import org.openscience.cdk.qsar.IDescriptor;
+import org.openscience.cdk.qsar.IMolecularDescriptor;
+import org.openscience.cdk.qsar.result.DoubleArrayResult;
+import org.openscience.cdk.qsar.result.DoubleResult;
+import org.openscience.cdk.qsar.result.IDescriptorResult;
+import org.openscience.cdk.qsar.result.IntegerArrayResult;
+import org.openscience.cdk.qsar.result.IntegerResult;
 
 import javax.swing.*;
 import javax.swing.border.Border;
@@ -37,13 +53,16 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.GregorianCalendar;
@@ -413,10 +432,174 @@ public class CDKdesc extends JFrame implements DropTargetListener {
     }
 
     private void batch(String inputFile, String outputFile, String descriptorType, boolean verbose) {
+
         if (verbose) {
             System.out.println("INFO: input:\t" + inputFile);
             System.out.println("INFO: output:\t" + outputFile);
             System.out.println("INFO: type:\t" + descriptorType);
+        }
+
+        DescriptorEngine engine = new DescriptorEngine(DescriptorEngine.MOLECULAR);
+        List<String> classNames = engine.getDescriptorClassNames();
+
+        List<String> validClassNames;
+        if (descriptorType.equals("all")) {
+            validClassNames = new ArrayList<String>(classNames);
+        } else {
+            validClassNames = new ArrayList<String>();
+            for (String className : classNames) {
+                String[] dictClasses = engine.getDictionaryClass(className);
+                if (dictClasses == null) continue;
+                for (String dictClass : dictClasses) {
+                    if (dictClass.indexOf(descriptorType) != -1) validClassNames.add(className);
+                }
+            }
+        }
+
+        if (verbose) System.out.println("INFO: Will evaluate " + validClassNames.size() + " descriptors");
+        List<IDescriptor> instances = engine.instantiateDescriptors(validClassNames);
+        if (verbose) System.out.println("INFO: Got " + validClassNames.size() + " descriptor instances");
+        engine.setDescriptorInstances(instances);
+
+        // ok, we've got the desc engine set up, lets check inputs and start the fun
+        String inputFormat = "invalid";
+        if (CDKDescUtils.isSMILESFormat(inputFile)) inputFormat = "smi";
+        else if (CDKDescUtils.isMDLFormat(inputFile)) inputFormat = "mdl";
+        else {
+            System.out.println("Currently only SMILES of SDF formats are supported");
+            System.exit(-1);
+        }
+
+        DefaultIteratingChemObjectReader iterReader = null;
+        BufferedWriter tmpWriter = null;
+        try {
+            tmpWriter = new BufferedWriter(new FileWriter(outputFile));
+
+            FileInputStream inputStream = new FileInputStream(inputFile);
+            if (inputFormat.equals("smi")) iterReader = new IteratingSMILESReader(inputStream);
+            else if (inputFormat.equals("mdl")) {
+                iterReader = new IteratingMDLReader(inputStream, DefaultChemObjectBuilder.getInstance());
+                iterReader.addChemObjectIOListener(new SDFormatListener());
+                ((IteratingMDLReader) iterReader).customizeJob();
+            }
+        } catch (IOException exception) {
+            System.out.println("ERROR: Error opening input file");
+            System.out.println(exception.toString());
+            System.exit(-1);
+        }
+
+        ITextOutput textOutput = new PlainTextOutput(tmpWriter);
+        textOutput.setItemSeparator("\t");
+
+
+        // lets get the header line first
+        List<String> headerItems = new ArrayList<String>();
+        headerItems.add("Title");
+        for (IDescriptor descriptor : instances) {
+            String[] names = descriptor.getDescriptorNames();
+            headerItems.addAll(Arrays.asList(names));
+        }
+
+        try {
+            assert textOutput != null;
+            textOutput.writeHeader(headerItems.toArray(new String[]{}));
+        } catch (IOException e) {
+            System.out.println("ERROR: Error writing header line");
+            System.out.println(e.toString());
+            System.exit(-1);
+        }
+
+        double elapsedTime = System.currentTimeMillis();
+        List<ExceptionInfo> exceptionList = new ArrayList<ExceptionInfo>();
+        int nmol = 0;
+
+        while (iterReader.hasNext()) {  // loop over molecules
+            IMolecule molecule = (IMolecule) iterReader.next();
+            String title = (String) molecule.getProperty(CDKConstants.TITLE);
+            if (title == null) title = "Mol" + String.valueOf(nmol + 1);
+
+            try {
+                molecule = (IMolecule) CDKDescUtils.checkAndCleanMolecule(molecule);
+            } catch (CDKException e) {
+                exceptionList.add(new ExceptionInfo(nmol + 1, molecule, e, ""));
+                nmol++;
+                continue;
+            }
+
+            // OK, we can now eval the descriptors
+            List<String> dataItems = new ArrayList<String>();
+            dataItems.add(title);
+
+            int ndesc = 0;
+            for (Object object : instances) {
+                IMolecularDescriptor descriptor = (IMolecularDescriptor) object;
+                String[] comps = descriptor.getSpecification().getSpecificationReference().split("#");
+
+                DescriptorValue value = descriptor.calculate(molecule);
+                if (value.getException() != null) {
+                    exceptionList.add(new ExceptionInfo(nmol + 1, molecule, value.getException(), comps[1]));
+                    for (int i = 0; i < value.getNames().length; i++)
+                        dataItems.add("NA");
+                    continue;
+                }
+
+                IDescriptorResult result = value.getValue();
+                if (result instanceof DoubleResult) {
+                    dataItems.add(String.valueOf(((DoubleResult) result).doubleValue()));
+                } else if (result instanceof IntegerResult) {
+                    dataItems.add(String.valueOf(((IntegerResult) result).intValue()));
+                } else if (result instanceof DoubleArrayResult) {
+                    for (int i = 0; i < ((DoubleArrayResult) result).length(); i++) {
+                        dataItems.add(String.valueOf(((DoubleArrayResult) result).get(i)));
+                    }
+                } else if (result instanceof IntegerArrayResult) {
+                    for (int i = 0; i < ((IntegerArrayResult) result).length(); i++) {
+                        dataItems.add(String.valueOf(((IntegerArrayResult) result).get(i)));
+                    }
+                }
+
+                ndesc++;
+
+                if (verbose) {
+                    System.out.print("\rINFO: Processed " + ndesc + " descriptors for " + nmol + " molecules");
+                    System.out.flush();
+                }
+            }
+
+            for (int i = 0; i < dataItems.size(); i++) {
+                if (dataItems.get(i).equals("NaN")) dataItems.set(i, "NA");
+            }
+
+            try {
+                textOutput.writeLine(dataItems.toArray(new String[]{}));
+            } catch (IOException e) {
+                System.out.println("\nERROR: Error writing data line");
+                System.out.println(e.toString());
+                System.exit(-1);
+            }
+
+            nmol++;
+        }
+
+        // calculation is done, lets eval the elapsed time
+        elapsedTime = ((System.currentTimeMillis() - elapsedTime) / 1000.0);
+
+        try {
+            iterReader.close();
+            tmpWriter.close();
+        } catch (IOException e) {
+            System.out.println("\nERROR: Error closing files");
+            System.out.println(e.toString());
+            System.exit(-1);
+        }
+
+        if (verbose) System.out.println("\nINFO: Completed in " + elapsedTime + " sec");
+
+        if (exceptionList.size() > 0) {
+            System.out.println("=============== Exceptions ===============");
+            for (ExceptionInfo ei : exceptionList) {
+                System.out.println(ei.getMolecule().getProperty(CDKConstants.TITLE) + " " + ei.getDescriptorName() + " " + ei.getException());
+            }
         }
     }
 
@@ -428,7 +611,7 @@ public class CDKdesc extends JFrame implements DropTargetListener {
     private static void usage(Options options) {
         HelpFormatter helpFormatter = new HelpFormatter();
         helpFormatter.printHelp("cdkdescui [OPTIONS] inputfile\n", options);
-        System.out.println("\nCDKDescUI v"+CDKDescConstants.VERSION+" Rajarshi Guha <rajarshi.guha@gmail.com>\n");
+        System.out.println("\nCDKDescUI v" + CDKDescConstants.VERSION + " Rajarshi Guha <rajarshi.guha@gmail.com>\n");
         System.exit(-1);
     }
 
@@ -462,7 +645,7 @@ public class CDKdesc extends JFrame implements DropTargetListener {
             if (cmd.hasOption("o")) outputFile = cmd.getOptionValue("o");
             if (cmd.hasOption("t")) {
                 descriptorType = cmd.getOptionValue("t");
-                String[] validTypes = new String[]{"all", "topological", "geometric", "constitutional", "electronic", "hybrid"};
+                String[] validTypes = new String[]{"all", "topological", "geometric", "protein", "constitutional", "electronic", "hybrid"};
                 boolean typeOk = false;
                 for (String s : validTypes) {
                     if (s.equals(descriptorType)) {
