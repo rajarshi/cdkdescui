224262af34ad73e4c4b9c1ec10cc3bc473b1089b
Rajarshi Guha <rajarshi.guha@gmail.com>
2014-03-08 13:06:12 -0500

    initial steps to mavenization

--- src/main/java/net/guha/apps/cdkdesc/CDKDescConstants.java
+++ src/main/java/net/guha/apps/cdkdesc/CDKDescConstants.java
@@ -5,7 +5,7 @@ package net.guha.apps.cdkdesc;
  */
 public class CDKDescConstants {
 
-    public static final String VERSION = "1.3.9";
+    public static final String VERSION = "1.4.0";
 
     public static final String OUTPUT_CSV = "CSV";
     public static final String OUTPUT_TAB = "TAB";
